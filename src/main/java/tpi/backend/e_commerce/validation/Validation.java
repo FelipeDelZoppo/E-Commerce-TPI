8dedf3ea26cbb54446b96487ec8f75c0d7409a1e
Felipe Del Zoppo <felipedelzoppo@gmail.com>
2024-11-08 21:15:13 -0300

    feat: Crear metodo que recibe un JWT y valida que el rol sea ADMIN. #72 #89

--- src/main/java/tpi/backend/e_commerce/validation/Validation.java
+++ src/main/java/tpi/backend/e_commerce/validation/Validation.java
@@ -1,14 +1,22 @@
 package tpi.backend.e_commerce.validation;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.BindingResult;
+
+import tpi.backend.e_commerce.enums.Role;
+import tpi.backend.e_commerce.services.JwtService.JwtService;
+
 import java.util.Map;
 import java.util.Collections;
 import java.util.HashMap;
 
 @Component
 public class Validation {
+    
+    @Autowired
+    private JwtService jwtService;
     
     public ResponseEntity<Map<String,String>> validate(BindingResult result){
         Map<String,String> errors = new HashMap<>();
@@ -21,6 +29,13 @@ public class Validation {
 
     public ResponseEntity<Map<String,String>> validate(String field, String message, int status){
         return ResponseEntity.status(status).body(Collections.singletonMap(field, message));
+    }
+
+    public void validateRole(String jwt)throws Exception {
+        Role role = jwtService.extractRole(jwt);
+        if (!role.equals(Role.ADMIN)) {
+            throw new RuntimeException("Solo los usuarios con rol ADMIN pueden registrar otro usuario ADMIN");
+        }
     }
     
 }
