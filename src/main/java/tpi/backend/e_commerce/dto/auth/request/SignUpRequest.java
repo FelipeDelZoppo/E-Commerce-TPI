1eebc3721ef722e80912c3756d30fd9def41f9fd
Felipe Del Zoppo <felipedelzoppo@gmail.com>
2024-11-08 21:20:11 -0300

    feat: Cambiar signUpRequest para que reciba el JWT y un booleano admin. #72 #89

--- src/main/java/tpi/backend/e_commerce/dto/auth/request/SignUpRequest.java
+++ src/main/java/tpi/backend/e_commerce/dto/auth/request/SignUpRequest.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -36,6 +37,10 @@ public class SignUpRequest {
     @Size(min = 8, max = 22, message = "Debe tener entre 8 y 22 caracteres")
     private String password;
 
-    private boolean isAdmin;
+    @NotNull
+    private String jwt;
+
+    @NotNull
+    private boolean admin;
 
 }
